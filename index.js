fb69ec7530c713ceab5de0a374f179e69759ae6a
lreboseno22 <liamrebo22@gmail.com>
2021-11-10 15:40:17 +0000

    Question complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //write your code here
+
+// iterate through the string
+// if letter is uppercase remove that letter return the string back with removed letters.
+// else just return the string back with.
+function stripUpperCase(str){
+    let stripped = '';
+    for (let i of str){
+        if(i === i.toLowerCase()){
+            stripped += i;
+        }
+    }
+    return stripped;
+    
+}
+console.log(stripUpperCase('Hello'));
